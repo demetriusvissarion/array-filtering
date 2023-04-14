cc3020fc1681499f410ea80d943acaa871404571
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-14 10:06:20 +0100

    array column

--- index.php
+++ index.php
@@ -4,20 +4,23 @@ class Post
 {
     public $title;
 
+    public $author;
+
     public $published;
 
-    public function __construct($title, $published)
+    public function __construct($title, $author, $published)
     {
         $this->title = $title;
+        $this->author = $author;
         $this->published = $published;
     }
 }
 
 $posts = [
-    new Post('My First Post', true),
-    new Post('My Second Post', true),
-    new Post('My Third Post', true),
-    new Post('My Fourth Post', false),
+    new Post('My First Post', 'JW', true),
+    new Post('My Second Post', 'JW', true),
+    new Post('My Third Post', 'AW', true),
+    new Post('My Fourth Post', 'TR', false),
 ];
 
 // $publishedPosts = array_filter($posts, function($post){
@@ -37,14 +40,31 @@ $posts = [
 // }, $posts);
 
 //// foreach
-foreach($posts as $post) {
-    $post->published = true;
-}
+// foreach($posts as $post) {
+//     $post->published = true;
+// }
 
 //// map usage
 // $modified = array_map(function ($post){
-//     $post->published = true;
-//     return $post;
+//     return ['title' => $post->title];
 // }, $posts);
 
-var_dump($posts);
+// $titles = array_map(function ($post) {
+//     return $post->title;
+// }, $posts);
+
+// $titles = array_column($posts, 'title'); // same result as array-map but cleaner, works only when "public"
+
+// foreach ($posts as $index => $post)
+// {
+//     $posts[$index] = (array) $post;
+// }
+
+$posts = array_map(function ($post){
+    return (array) $post;
+}, $posts);
+
+// $titles = array_column($posts, 'title');
+$authors = array_column($posts, 'author');
+
+var_dump($authors);
