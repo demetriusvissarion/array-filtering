74ee9c8b9a03690e45f912aafa6fe7212502f275
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-14 09:26:29 +0100

    array filter

--- index.php
+++ index.php
@@ -19,3 +19,13 @@ $posts = [
     new Post('My Third Post', true),
     new Post('My Fourth Post', false),
 ];
+
+$publishedPosts = array_filter($posts, function($post){
+    return $post->published;
+});
+
+$unpublishedPosts = array_filter($posts, function($post){
+    return !$post->published;
+});
+
+var_dump($publishedPosts);
